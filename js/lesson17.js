b8282ff15b7558590e16f62ab1e725983d080241
“trulytylergit config --global user.email “tylerharrisdesign@gmail.com <YOUR@EMAIL.com>
2016-06-28 20:09:29 -0700

    hello again

--- js/lesson17.js
+++ js/lesson17.js
@@ -1,63 +1,63 @@
-$(document).ready(function() {
-  // create object instance of my Firebase database
-  var myDBReference = new Firebase('https://supertest-4733c.firebaseio.com/');
+// $(document).ready(function() {
+//   // create object instance of my Firebase database
+//   var myDBReference = new Firebase('https://supertest-4733c.firebaseio.com/');
 
-  var sourceTemplate = $('#list-template').html();
-  var template = Handlebars.compile(sourceTemplate);
+//   var sourceTemplate = $('#list-template').html();
+//   var template = Handlebars.compile(sourceTemplate);
 
-  // define submit event listener/handler
-  $('#message-form').submit(function(event) {
-    // prevents page refresh
-    event.preventDefault();
+//   // define submit event listener/handler
+//   $('#message-form').submit(function(event) {
+//     // prevents page refresh
+//     event.preventDefault();
 
-    // grab user input
-    var message = $('#message').val();
-    var calculation = $('#calculation').val();
+//     // grab user input
+//     var message = $('#message').val();
+//     var calculation = $('#calculation').val();
 
 
 
-    var messagesReference = myDBReference.child('messages');
-    messagesReference.push({
-      message: message
-    });
-  });
+//     var messagesReference = myDBReference.child('messages');
+//     messagesReference.push({
+//       message: message
+//     });
+//   });
 
-  // Read functionality
-  myDBReference.child('messages').on('child_added', function(results) {
-    results.forEach(function(message) {
+//   // Read functionality
+//   myDBReference.child('messages').on('child_added', function(results) {
+//     results.forEach(function(message) {
 
-      var data = {
-        message: message.val(),
-        id: results.key()
-      };
+//       var data = {
+//         message: message.val(),
+//         id: results.key()
+//       };
 
-      var templateHTML = template(data);
+//       var templateHTML = template(data);
 
-      var $templateHTML = $(templateHTML);
+//       var $templateHTML = $(templateHTML);
 
-      $templateHTML.click(function() {
-        var messageId = $(this).data('id');
-        deleteMessage(messageId);
-      })
-      $('#messages-list').append($templateHTML);
-    });
-  });
+//       $templateHTML.click(function() {
+//         var messageId = $(this).data('id');
+//         deleteMessage(messageId);
+//       })
+//       $('#messages-list').append($templateHTML);
+//     });
+//   });
 
-  // // Update Functionality
-  // function updateMessage(id, message) {
-  //   var messageReference = new Firebase('https://supertest-4733c.firebaseio.com/messages/' + id);
+//   // // Update Functionality
+//   // function updateMessage(id, message) {
+//   //   var messageReference = new Firebase('https://supertest-4733c.firebaseio.com/messages/' + id);
 
-  //   messageReference.update({
-  //     message: 'fu man shu'
-  //   });
+//   //   messageReference.update({
+//   //     message: 'fu man shu'
+//   //   });
 
-  // }
+//   // }
 
-  // Delete functionality
-  function deleteMessage(id, message) {
-    var messageReference = new Firebase('https://supertest-4733c.firebaseio.com//messages/' + id);
+//   // Delete functionality
+//   function deleteMessage(id, message) {
+//     var messageReference = new Firebase('https://supertest-4733c.firebaseio.com//messages/' + id);
 
-    messageReference.remove();
-  }
+//     messageReference.remove();
+//   }
 
-});
+// });
