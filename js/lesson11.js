e968107a954b526d7874f4ae46283dbcdd172674
“trulytylergit config --global user.email “tylerharrisdesign@gmail.com <YOUR@EMAIL.com>
2016-06-28 20:40:52 -0700

    hh

--- js/lesson11.js
+++
@@ -1,9 +0,0 @@
-//creating an http request http://www.w3schools.com/xml/dom_httprequest.asp?
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-	console.log("whats up");
-};
-xhttp.open('GET', 'http://data.consumerfinance.gov/api/views.json');
-xhttp.send();
-
-
